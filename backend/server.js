38932b26befab2a44bd425fe332db8e924ff8f70
gokulkrishnaskumbla <gokulkrishnaskumbla@gmail.com>
2025-11-15 02:37:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://fixora-services.onrender.com',
   credentials: true,
   methods :['get','post','put','delete','patch']               
 }));
