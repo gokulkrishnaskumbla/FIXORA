f316461b7f7e2e258b62ae326d45c03e4c314aab
gokulkrishnaskumbla <gokulkrishnaskumbla@gmail.com>
2025-11-15 03:14:22 +0530

    Update Navbar.jsx

--- frontend/fixora/src/components/Navbar.jsx
+++ frontend/fixora/src/components/Navbar.jsx
@@ -94,9 +94,10 @@ function Navbar() {
   <img
     src="/Gemini_Generated_Image_gdzs6fgdzs6fgdzs-removebg-preview.png"
     alt="Fixora Logo"
-    className="h-30 w-auto object-contain"
+    className="h-12 sm:h-16 md:h-20 w-auto object-contain"
   />
 </a>
+
 
       </div>
 
